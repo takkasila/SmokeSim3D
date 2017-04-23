9563ad958853c2334592acbd98a78cccff51a388
takkasila <takkasila@outlook.com>
2017-04-23 20:55:31 +0700

    Changed volume marching to voxel traversal.

--- src/glsl/render-frag.glsl
+++ src/glsl/render-frag.glsl
@@ -11,9 +11,10 @@ uniform float u_screen_height;
 varying vec2 f_uv;
 
 #define SCENEBB_POS vec3(0.0, -1.0, 0.0)
-#define SCENEBB_SIZE vec3(5.0, 0.8, 5.0)
-#define SMOKEBB_POS vec3(0.5)
-#define SMOKEBB_SIZE vec3(0.5)
+#define SCENEBB_SIZE vec3(5.0, 1.0, 5.0)
+#define SMOKEBB_START vec3(-5.0, 0.0, -5.0)
+#define SMOKEBB_STOP vec3(5.0, 10.0, 5.0)
+#define CELL_COUNT ivec3(10)
 
 struct Ray {
     vec3 start;
@@ -27,10 +28,20 @@ float checkerboardTexture(vec3 p)
     return mod(p.x+p.y+p.z, 2.0);
 }
 
+float smokeDense(vec3 p)
+{
+    if(abs(p.x) < 2.0 && abs(p.z) <2.0)
+        return 1.0;
+    else if(p.y >7.0)
+        return 1.0;
+    else
+        return 0.1;
+}
+
 vec3 absorb(vec3 originColor, float amount)
 {
-    float fogAmount = 1.0 - exp(-amount * 1.0);
-    vec3 fogColor = vec3(0.5, 0.6, 0.7);
+    float fogAmount = 1.0 - exp(-amount * 3.0);
+    vec3 fogColor = vec3(1.0, 0.0, 0.0);
     return mix(originColor, fogColor, fogAmount);
 }
 
@@ -63,6 +74,46 @@ float intersectRayCube(vec3 rp, vec3 rd, vec3 cp, vec3 cth, out vec2 t)
 	return t.y < t.x ? 0.0 : (t.x > 0.0 ? 1.0 : -1.0);
 }
 
+float intersectRayCubeNorm(vec3 rp, vec3 rd, vec3 cp, vec3 cth, out vec2 t, out vec3 n0, out vec3 n1)
+{	
+	rp -= cp;
+	
+	vec3 m = 1.0 / -rd;
+    vec3 os = If(lessThan(rd, vec3(0.0)), vec3(1.0), vec3(-1.0));
+    //vec3 os = sign(-rd);
+	vec3 o = -cth * os;
+	
+    
+	vec3 uf = (rp + o) * m;
+	vec3 ub = (rp - o) * m;
+	
+	//t.x = max(uf.x, max(uf.y, uf.z));
+	//t.y = min(ub.x, min(ub.y, ub.z));
+	
+    if(uf.x > uf.y) {t.x = uf.x; n0 = vec3(os.x, 0.0, 0.0);} else 
+                    {t.x = uf.y; n0 = vec3(0.0, os.y, 0.0);}
+    if(uf.z > t.x ) {t.x = uf.z; n0 = vec3(0.0, 0.0, os.z);}
+    
+    if(ub.x < ub.y) {t.y = ub.x; n1 = vec3(os.x, 0.0, 0.0);} else 
+                    {t.y = ub.y; n1 = vec3(0.0, os.y, 0.0);}
+    if(ub.z < t.y ) {t.y = ub.z; n1 = vec3(0.0, 0.0, os.z);}
+    
+    
+	// if(ray start == inside cube) 
+	if(t.x < 0.0 && t.y > 0.0) 
+    {
+        t.xy = t.yx;  
+        
+        vec3 n00 = n0;
+        n0 = n1;
+        n1 = n00;
+        
+        return 1.0;
+    }
+	
+	return t.y < t.x ? 0.0 : (t.x > 0.0 ? 1.0 : -1.0);
+}
+
 vec3 rayDirection()
 {
     // Creat camera plane
@@ -76,25 +127,82 @@ vec3 rayDirection()
     return normalize(-view_n * u_cam_near + plane_right * uv.x + plane_top * uv.y);
 }
 
+// Volxel traversal along 3D line
 float densityTrace(Ray ray)
 {
+    float totalDense = 0.0;
+    vec3 cellSize = (SMOKEBB_STOP - SMOKEBB_START) / float(CELL_COUNT);
     vec2 tt;
-    if(intersectRayCube(ray.start, ray.dir, SMOKEBB_POS, SMOKEBB_SIZE, tt) > 0.0)
+    vec3 n0, n1;
+    int x, y, z, dx, dy, dz;
+    if(intersectRayCubeNorm(ray.start, ray.dir, (SMOKEBB_START + SMOKEBB_STOP)/2.0, (SMOKEBB_STOP - SMOKEBB_START)/2.0, tt, n0, n1) > 0.0)
     {
-        return abs(tt.x-tt.y);
+        // Enter cube to reduce surface error
+        vec3 startPos = ray.start + ray.dir * tt.x - n0 * 0.01;
+        x = int(floor(startPos.x));
+        y = int(floor(startPos.y));
+        z = int(floor(startPos.z));
+        vec3 endPos = ray.start + ray.dir * tt.y + n1 * 0.01;
+        dx = int(floor(endPos.x));
+        dy = int(floor(endPos.y));
+        dz = int(floor(endPos.z));
     }
     else
-    {
         return 0.0;
+        
+    int sx, sy, sz, exy, exz, ezy, ax, ay, az, bx, by, bz;
+    sx = int(sign(float(dx)));
+    sy = int(sign(float(dy)));
+    sz = int(sign(float(dz)));
+    ax = int(abs(float(dx)));
+    ay = int(abs(float(dy)));
+    az = int(abs(float(dz)));
+    bx = 2*ax;	   by = 2*ay;	  bz = 2*az;
+    exy = ay-ax;   exz = az-ax;	  ezy = ay-az;
+
+    for(int n = 0; n < CELL_COUNT.x + CELL_COUNT.y + CELL_COUNT.z; n++)
+    {
+        if(n >= ax+ay+az)
+            break;
+
+        vec3 cellPos = SMOKEBB_START + vec3(x,y,z)*cellSize + cellSize/2.0;
+        if(intersectRayCube(ray.start, ray.dir, cellPos, cellSize/2.0, tt) > 0.0)
+            totalDense += smokeDense(cellPos);
+        
+        // Update
+        if ( exy < 0 ) {
+            if ( exz < 0 ) {
+            x += sx;
+            exy += by; exz += bz;
+            }
+            else  {
+            z += sz;
+            exz -= bx; ezy += by;
+            }
+        }
+        else {
+            if ( ezy < 0 ) {
+            z += sz;
+            exz -= bx; ezy += by;
+            }
+            else  {
+            y += sy;
+            exy -= bx; ezy -= bz;
+            }
+        }
     }
+    // return totalDense;
+    return float(ax+ay+az)/float(CELL_COUNT.x + CELL_COUNT.y + CELL_COUNT.z);
+    // return 1.0;
 }
 
 vec3 renderScene(Ray ray)
 {
     vec2 tt;
-    if(intersectRayCube(ray.start, ray.dir, SCENEBB_POS, SCENEBB_SIZE, tt) > 0.0)
+    vec3 n0, n1;
+    if(intersectRayCubeNorm(ray.start, ray.dir, SCENEBB_POS, SCENEBB_SIZE, tt, n0, n1) > 0.0)
     {
-        return vec3(checkerboardTexture(ray.start + ray.dir * tt.x));
+        return vec3(checkerboardTexture(ray.start + ray.dir * tt.x - n0 * 0.01));
     }
     else
     {
@@ -115,7 +223,7 @@ vec3 render(Ray ray)
     if (intersectRayCube(ray.start, ray.dir, SCENEBB_POS, SCENEBB_SIZE, sceneDist) > 0.0)
     {
         vec3 sceneColor = renderScene(ray);
-        if(intersectRayCube(ray.start, ray.dir, SMOKEBB_POS, SMOKEBB_SIZE, smokeDist) > 0.0)
+        if(intersectRayCube(ray.start, ray.dir, (SMOKEBB_START + SMOKEBB_STOP)/2.0, (SMOKEBB_STOP-SMOKEBB_START)/2.0, smokeDist) > 0.0)
         {
             // Hit both
             if(sceneDist.x <= smokeDist.x)
@@ -137,7 +245,7 @@ vec3 render(Ray ray)
     else
     {
         vec3 skyboxColor = renderSkyBox(ray);
-        if(intersectRayCube(ray.start, ray.dir, SMOKEBB_POS, SMOKEBB_SIZE, smokeDist) > 0.0)
+        if(intersectRayCube(ray.start, ray.dir, (SMOKEBB_START + SMOKEBB_STOP)/2.0, (SMOKEBB_STOP-SMOKEBB_START)/2.0, smokeDist) > 0.0)
         {
             // Hit smoke
             float totalDense = densityTrace(ray);
@@ -156,8 +264,6 @@ void main() {
     ray.start = u_cam_pos;
     ray.dir = rayDirection();
     ray.depth = 0.0;
-
-    vec3 sceneColor = renderScene(ray);
 
     gl_FragColor = vec4(render(ray), 1.0);
 }
